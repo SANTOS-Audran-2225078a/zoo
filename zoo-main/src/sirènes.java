248a6f8ae86443e4755f7826c9856787fc1d186e
CHOPARD-William-2225020 <123471387+WilliamCHOPARDC22003913@users.noreply.github.com>
2023-11-17 09:39:47 +0100

    Add files via upload

---
+++ zoo-main/src/sirènes.java
@@ -0,0 +1,29 @@
+package td4;
+
+public class sirènes {
+	 private String nomEspece;
+	    private char sexe;
+	    private double poids;
+	    private double taille;
+	    private int age;
+	    private boolean indicfaim; 
+	    private boolean indicsommeil;
+	    private boolean indicsante;
+
+	        public sirènes(String nomEspece, char sexe, double poids, double taille, int age) {
+	        this.nomEspece = nomEspece;
+	        this.sexe = sexe;
+	        this.poids = poids;
+	        this.taille = taille;
+	        this.age = age;
+	        this.indicfaim = false;
+	        this.indicsommeil = false;
+	        this.indicsante = true;
+	    }
+
+	 @override
+    public void Nager() {
+        System.out.println(super.getName() + "nage" );
+    }
+
+}
