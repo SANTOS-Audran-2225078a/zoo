f69c64be75dcfeb994733ef6c84efe5c24bc97c8
Audran <115973951+SANTOS-Audran-2225078a@users.noreply.github.com>
2023-11-28 11:05:56 +0100

    Update Megalodon.java

--- zoo-main/src/lasauce/Megalodon.java
+++ zoo-main/src/lasauce/Megalodon.java
@@ -87,5 +87,105 @@ public class Megalodon {
     }
 
 
-}
+	public String getNomEspece() {
+		return nomEspece;
+	}
+
+
+	public void setNomEspece(String nomEspece) {
+		this.nomEspece = nomEspece;
+	}
+
+
+	public String getSexe() {
+		return sexe;
+	}
+
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+
+	public double getPoids() {
+		return poids;
+	}
+
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+
+	public double getTaille() {
+		return taille;
+	}
+
+
+	public void setTaille(double taille) {
+		this.taille = taille;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public boolean isFaim() {
+		return faim;
+	}
+
+
+	public void setFaim(boolean faim) {
+		this.faim = faim;
+	}
+
+
+	public boolean isDort() {
+		return dort;
+	}
+
+
+	public void setDort(boolean dort) {
+		this.dort = dort;
+	}
+
+
+	public int getSante() {
+		return sante;
+	}
+
+
+	public void setSante(int sante) {
+		this.sante = sante;
+	}
+
+
+	public boolean isNager() {
+		return nager;
+	}
+
+
+	public void setNager(boolean nager) {
+		this.nager = nager;
+	}
+
+
+	public boolean isPond() {
+		return pond;
+	}
+
+
+	public void setPond(boolean pond) {
+		this.pond = pond;
+	}
+
+
+
 }
