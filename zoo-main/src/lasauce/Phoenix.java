b661fd0939343777ab93f3e7563f2bfd39d7a86f
Audran <115973951+SANTOS-Audran-2225078a@users.noreply.github.com>
2023-11-28 11:06:32 +0100

    Update Phoenix.java

--- zoo-main/src/lasauce/Phoenix.java
+++ zoo-main/src/lasauce/Phoenix.java
@@ -99,6 +99,116 @@ public class Phoenix {
 	}
 
 
+	public String getNomEspece() {
+		return nomEspece;
+	}
 
-}
+
+	public void setNomEspece(String nomEspece) {
+		this.nomEspece = nomEspece;
+	}
+
+
+	public String getSexe() {
+		return sexe;
+	}
+
+
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+
+
+	public double getPoids() {
+		return poids;
+	}
+
+
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}
+
+
+	public double getTaille() {
+		return taille;
+	}
+
+
+	public void setTaille(double taille) {
+		this.taille = taille;
+	}
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+	public boolean isFaim() {
+		return faim;
+	}
+
+
+	public void setFaim(boolean faim) {
+		this.faim = faim;
+	}
+
+
+	public boolean isDort() {
+		return dort;
+	}
+
+
+	public void setDort(boolean dort) {
+		this.dort = dort;
+	}
+
+
+	public int getSante() {
+		return sante;
+	}
+
+
+	public void setSante(int sante) {
+		this.sante = sante;
+	}
+
+
+	public boolean isVoler() {
+		return voler;
+	}
+
+
+	public void setVoler(boolean voler) {
+		this.voler = voler;
+	}
+
+
+	public boolean isPond() {
+		return pond;
+	}
+
+
+	public void setPond(boolean pond) {
+		this.pond = pond;
+	}
+
+
+	public boolean isRenaitre() {
+		return renaitre;
+	}
+
+
+	public void setRenaitre(boolean renaitre) {
+		this.renaitre = renaitre;
+	}
+
+
+
+
 }
