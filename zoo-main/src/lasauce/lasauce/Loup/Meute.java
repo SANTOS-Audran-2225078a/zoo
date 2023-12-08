ebb41dbeb78feb62262ad9e19464a8fb562684dd
Asugra84 <115951862+Asugra84@users.noreply.github.com>
2023-12-08 21:45:28 +0100

    Update Meute.java

--- zoo-main/src/lasauce/lasauce/Loup/Meute.java
+++ zoo-main/src/lasauce/lasauce/Loup/Meute.java
@@ -1,5 +1,17 @@
 package lasauce.Loup;
 
+import java.util.ArrayList;
+
 public class Meute {
     private String meute;
+    public Meute() {
+
+    }
+    public Meute(String nomMeute) {
+        super();
+        this.meute = nomMeute;
+    }
+    public String getAllMeuteName() {
+        return meute;
+    }
 }
